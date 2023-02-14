60f76b0d507cc5b490d79509c9dcde10147d1188
Vaek <timothe.boulet@student-cs.fr>
2023-02-14 02:45:49 -0500

    version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "matplotlib",
         "tqdm",
     ],
-    version="1.2",
+    version="1.3.0",
     license="MIT",
     description="Measure of python performance in local.",
     long_description=open('README.md').read(),      
